74081691df14c6750cc9ca59c9c6312199198e2e
Aidan Kennedy <josephscottsnedeker@gmail.com>
2019-01-26 12:33:45 -0700

    Hands

---
+++ FamilySim/Assets/Scripts/CubeObjectScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeObjectScript : MonoBehaviour
+{
+    Rigidbody cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+
+            Debug.Log("Backspace");
+            cube.transform.position = Vector3.zero;
+            cube.velocity = Vector3.zero;
+
+        }
+    }
+}
