ee0eed81a4362eb2d115517ee883037251539434
JosephSnedeker1 <josephscottsnedeker@gmail.com>
2019-01-25 21:41:47 -0700

    started game jam project and set up cam

---
+++ FamilySim/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+    public int camNumber { get; set; }
+    static GameManager instance;
+
+    public Rigidbody player { get; set; }
+
+    public Vector3 look { get; set; }
+
+   
+
+    public List<Camera> ballCams { get; set; } = new List<Camera>();
+    
+     
+
+    public int ballCount = 0;
+
+
+    public static GameManager Instance
+    { get { return instance ?? ((instance = new GameManager())); } }
+
+    private GameManager()
+    {
+        Object.DontDestroyOnLoad(new GameObject("Updater", typeof(Updater)));
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) || Input.GetKeyDown(KeyCode.X))
+        {
+            ballCams[camNumber].enabled = false;
+            camNumber += 1;
+            if (camNumber == ballCams.Count)
+            {
+                camNumber = 0;
+            }
+            ballCams[camNumber].enabled = true;
+        }
+    }
+    class Updater : MonoBehaviour
+    {
+        private void Update()
+        {
+            instance.Update();
+        }
+    }
+}  
+
