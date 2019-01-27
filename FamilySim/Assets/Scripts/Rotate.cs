89a9146a741a588c34f6e9621ec405575ccc9dba
Aidan Kennedy <josephscottsnedeker@gmail.com>
2019-01-27 00:14:14 -0700

    Rotating player with thumbsticks

---
+++ FamilySim/Assets/Scripts/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x * 4, 0));
+        transform.Rotate(new Vector3(0, OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x * 4, 0));
+    }
+}
