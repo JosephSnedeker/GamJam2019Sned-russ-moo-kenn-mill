45272283517bf60cf867a90582cf829cd902c0d9
RussAMR <195712@student.asd20.org>
2019-01-26 21:43:30 -0700

    Secret Wall Addition.

---
+++ FamilySim/Assets/Scripts/Secret_Wall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Secret_Wall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.One) && OVRInput.Get(OVRInput.Button.Three))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
