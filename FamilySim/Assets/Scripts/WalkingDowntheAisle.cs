32ddf0ccf1b5f52ef5eb943cb433caa1e910439f
Aidan Kennedy <josephscottsnedeker@gmail.com>
2019-01-27 14:08:00 -0700

    More Objectives

--- FamilySim/Assets/Scripts/WalkingDowntheAisle.cs
+++ FamilySim/Assets/Scripts/WalkingDowntheAisle.cs
@@ -6,14 +6,17 @@ public class WalkingDowntheAisle : MonoBehaviour
 {
     private TeleportingLeft teleportLeft;
     private TeleportingRight teleportRight;
+    private Rotate rotate;
     private float timeLeft = 5;
     private bool waited = false;
+    private bool done = false;
     // Start is called before the first frame update
     void Start()
     {
 
         teleportRight = transform.GetChild(1).GetComponent<TeleportingRight>();
         teleportLeft = transform.GetChild(2).GetComponent<TeleportingLeft>();
+        rotate = transform.GetComponent<Rotate>();
         //teleportRight = transform.Find("RightHand").GetComponent<TeleportingRight>();
 
     }
@@ -29,7 +32,7 @@ public class WalkingDowntheAisle : MonoBehaviour
 
         }
 
-        if (transform.position.x > -33 && waited)
+        if (transform.position.x > -33 && waited && !done)
         {
 
             transform.position = new Vector3(transform.position.x - 0.04f, transform.position.y, transform.position.z);
@@ -37,9 +40,11 @@ public class WalkingDowntheAisle : MonoBehaviour
         }
         if (transform.position.x <= -33 && waited)
         {
-        
+
+            done = true;
             teleportLeft.enabled = true;
             teleportRight.enabled = true;
+            rotate.enabled = true;
 
         }
 
